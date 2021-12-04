641dbf002106003485f4898d4f24b86c8407e652
trungho <khanhtrungho1411@gmail.com>
2021-12-04 15:38:23 +0200

    p3

--- p3a/index.js
+++ p3a/index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(express.static('build'))
 morgan.token('body', (req, res)=> JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
